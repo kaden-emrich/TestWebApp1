6724e333acad4e96ab2be4b07f96e69e0b2966ba
Kaden Emrich <kadenemrich@gmail.com>
2023-09-19 13:17:29 -0400

    test1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>TEST1</title>
+
+</head>
+
+<body>
+
+    <p><?php echo 'Hello world!'; ?></p>
+    
+</body>
+
+</html>
